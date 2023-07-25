6036b11e47c77d4f5ff65e87e45faab84e11f655
ted2xmen <tugrulerdemdogru@gmail.com>
2023-07-25 23:21:43 +0300

    metadata fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,24 @@
-import "./globals.css";
-import type { Metadata } from "next";
+import './globals.css'
+import type { Metadata } from 'next'
 import { Roboto } from "next/font/google";
-import Footer from "./components/Footer";
+import Footer from './components/Footer';
 import Head from "next/head";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import { Analytics } from "@vercel/analytics/react";
+
 
 const roboto = Roboto({ subsets: ["latin"], weight: "500" });
 
-const title = "Articles and Tools for Javascript Developers";
-const description =
-  "Useful Articles and Tools for Javascript Developers - curated by Ted2xmen";
-
 export const metadata: Metadata = {
-  title,
-  description,
+  title: "Articles and Tools for Javascript Developers",
+  description: "Useful Articles and Tools for Javascript Developers - curated by Ted2xmen",
 };
-
-
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className="scroll-smooth">
